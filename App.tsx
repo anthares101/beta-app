f55103a54f6be52f02be7e94321129ea9e3de0af
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2022-06-10 18:57:59 +0200

    Start adding the message decoding process

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {StyleSheet, Text, ToastAndroid, View} from 'react-native';
 import {runOnJS} from 'react-native-reanimated';
 import {
@@ -8,28 +8,111 @@ import {
 } from 'react-native-vision-camera';
 import {scanOCR} from 'vision-camera-ocr';
 
+const SPECIAL_SEQUENCE = '99';
+
 const App = () => {
   const [hasPermission, setHasPermission] = useState(false);
-  const [detectedText, setDetectedText] = React.useState<any>();
+  const [detectedText, setDetectedText] = useState<any>();
   const devices = useCameraDevices();
   const device = devices.back;
+
+  const readingMessage = useRef(false);
+  const sequenceRead = useRef<string[]>([]);
 
   useEffect(() => {
     getCameraPermissions();
   }, []);
 
-  useEffect(() => {
-    if (detectedText && detectedText.result.text) {
-      ToastAndroid.show(
-        `Detected text: ${detectedText.result.text}`,
-        ToastAndroid.SHORT,
-      );
-    }
-  }, [detectedText]);
-
   const getCameraPermissions = async () => {
     const status = await Camera.requestCameraPermission();
     setHasPermission(status === 'authorized');
+  };
+
+  useEffect(() => {
+    if (isDetectedTextValid()) {
+      processDetectedText();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [detectedText]);
+
+  const isDetectedTextValid = (): boolean => {
+    return detectedText && detectedText.result.text.length >= 2;
+  };
+
+  const processDetectedText = () => {
+    let fpsValue = extractFpsValue();
+    if (!fpsValue) {
+      return;
+    }
+    fpsValue = fixFpsValue(fpsValue);
+    decodeMessage(fpsValue);
+  };
+
+  const extractFpsValue = (): number | undefined => {
+    const matches: string[] = detectedText.result.text.match(/\d+/g);
+    if (!matches) {
+      return;
+    }
+
+    const possibleFpsValues: number[] = matches
+      .filter(match => {
+        return Number(match) > 10 && Number(match) < 65;
+      })
+      .map(strNumber => {
+        return Number(strNumber);
+      });
+
+    if (possibleFpsValues.length === 0) {
+      return;
+    }
+
+    return Math.max(...possibleFpsValues);
+  };
+
+  const fixFpsValue = (fpsValue: number): number => {
+    const numbers = fpsValue.toString().split('');
+    if (numbers.length < 2) {
+      return fpsValue;
+    }
+
+    switch (numbers[1]) {
+      case '7': {
+        numbers[1] = '1';
+      }
+    }
+
+    return Number(numbers.join(''));
+  };
+
+  const decodeMessage = (fpsValue: number) => {
+    const bitDetected = fpsValue.toString().split('')[1];
+
+    if (!readingMessage.current) {
+      sequenceRead.current.push(bitDetected);
+      if (sequenceRead.current.length === 2) {
+        if (sequenceRead.current.join('') !== SPECIAL_SEQUENCE) {
+          sequenceRead.current.reverse().pop();
+          return;
+        }
+        ToastAndroid.show(
+          'Start sequence detected, decoding...',
+          ToastAndroid.SHORT,
+        );
+        readingMessage.current = true;
+        sequenceRead.current = [];
+        return;
+      }
+    }
+
+    sequenceRead.current.push(bitDetected);
+    if (sequenceRead.current.join('').slice(-2) === SPECIAL_SEQUENCE) {
+      ToastAndroid.show(
+        `Message received: ${sequenceRead.current.join('').slice(0, -2)}`,
+        ToastAndroid.SHORT,
+      );
+      readingMessage.current = false;
+      sequenceRead.current = [];
+    }
   };
 
   const frameProcessor = useFrameProcessor(frame => {
